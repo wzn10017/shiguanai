b3a16dda1cc871191ada33a531c9b84bb3be32c5
wzn10017 <156777360+wzn10017@users.noreply.github.com>
2024-01-16 20:31:39 +0800

    Delete seg directory

--- seg/t4.py
+++
@@ -1,7 +0,0 @@
-import openslide
-from openslide import deepzoom
-import matplotlib.pyplot as plt
-with openslide.OpenSlide(r"D:\dataset\cancer\C2023-11-02 01_16_39.svs") as slide:
-    thumbnail = slide.get_thumbnail((256, 256))
-    plt.imshow(thumbnail)
-plt.show()
